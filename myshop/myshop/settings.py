3943d8c66b2faa253646c85c18ee49da7252192d
Ryan <ryanlarkin0@gmail.com>
2024-10-07 17:16:15 -0700

    celery init

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -137,3 +137,11 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CART_SESSION_ID = 'cart'
+
+# email server config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
