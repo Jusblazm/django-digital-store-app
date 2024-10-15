769267682dd89d5888b52f8ef0f737b95c47524b
Ryan <ryanlarkin0@gmail.com>
2024-10-14 23:26:00 -0700

    payment app init

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     # my apps
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
     'shop.apps.ShopConfig',
 
     # default django apps
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,5 +1,5 @@
 from cart.cart import Cart
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import OrderCreateForm
 from .models import OrderItem
 from .tasks import order_created
@@ -21,9 +21,10 @@ def order_create(request):
             cart.clear()
             # launch async task
             order_created.delay(order.id)
-            return render(request,
-                          'orders/order/created.html',
-                          {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect('payment:process')
     else:
         form = OrderCreateForm()
     return render(request,
