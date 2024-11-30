33a64d944cddbb8419f43285a8760e48583d0779
Ryan <ryanlarkin0@gmail.com>
2024-11-30 08:31:32 -0800

    admin controls / csv

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,11 +1,55 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 # Register your models here.
 
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+order_payment.short_description = 'Stripe payment'
+
 class OrderItemInLine(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = (
+        f'attachment; filename={opts.verbose_name}.csv'
+    )
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    # write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -17,7 +61,10 @@ class OrderAdmin(admin.ModelAdmin):
                     'postal_code',
                     'city',
                     'paid',
+                    'order_payment',
                     'created',
-                    'updated']
+                    'updated',
+                    "order_detail"]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInLine]
+    actions = [export_to_csv]
